d3bd98d21566770f67e33f8ceacaf24d70094806
jndan21 <118275197+Eschatos21xx@users.noreply.github.com>
2024-08-06 17:13:54 +0700

    Tugas Modul Pemrograman Visual

---
+++ PemrogramanVisual/Coba2/src/main/java/com/mycompany/coba2/Coba2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.coba2;
+
+/**
+ *
+ * @author halif
+ */
+public class Coba2 {
+
+    public static void main(String[] args) {
+       Percobaan2 coba=new Percobaan2();
+       coba.setVisible(true);
+        System.out.println("Hello World!");
+    }
+}
---
+++ PemrogramanVisual/Formcobaa/src/main/java/com/mycompany/formcobaa/Formcobaa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.formcobaa;
+
+/**
+ *
+ * @author halif
+ */
+public class Formcobaa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
